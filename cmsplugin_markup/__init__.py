8a5d666cec587134163bf6e5e2e1250a1dd9ca86
Mitar <mitar.git@tnode.com>
2012-06-03 10:42:15 +0200

    Fixed version generation.

--- cmsplugin_markup/__init__.py
+++ cmsplugin_markup/__init__.py
@@ -3,13 +3,14 @@ __version__ = '.'.join(map(str, VERSION))
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
+    if len(VERSION) > 2 and VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+    if len(VERSION) > 3:
+        if VERSION[3:] == ('alpha', 0):
+            version = '%s pre-alpha' % version
+        else:
+            if VERSION[3] != 'final':
+                version = '%s %s %s' % (version, VERSION[3], VERSION[4])
     return version
 
 # patch settings
