97af7277a0e811e2d036c13e31c4d1b49c75ed0e
Mitar <mitar.git@tnode.com>
2012-06-03 19:38:33 +0200

    Escape JavaScript.

--- cmsplugin_markup/utils/media.py
+++ cmsplugin_markup/utils/media.py
@@ -1,13 +1,15 @@
+from django.utils.html import escapejs
+
 def content_scripts(scripts):
     output = ""
     for script in scripts:
-        output += '<script type="%s" src="%s"></script>' % (script.get('type', "text/javascript"), script['href'])
+        output += '<script type="%s" src="%s"></script>' % (script.get('type', 'text/javascript'), script['href'])
     return output
 
 def content_stylesheets(stylesheets):
     output = ""
     for stylesheet in stylesheets:
-        output += 'jQuery.loadStyleSheet("%s", "%s");\n' % (stylesheet['href'], stylesheet.get('type', "text/css"))
+        output += 'jQuery.loadStyleSheet("%s", "%s");\n' % (escapejs(stylesheet['href']), escapejs(stylesheet.get('type', 'text/css')))
     if output:
         output = '<script type="text/javascript">\n' + output + '</script>\n'
     return output
