c0ef1a19cda13423d17967c46d58cce83fbbb1ae
Guilherme Caminha <gpkc@cin.ufpe.br>
2016-07-08 18:53:11 -0300

    Fixing typos

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,11 +8,11 @@ class BaseTest(TestCase):
     pass
 
 class ModelTest(BaseTest):
-    def test_textile(self):
+    def test_markdown(self):
         instance = MarkupField.objects.create(body="# Markdown Markup!", markup="markdown")
         self.assertEqual(instance.body_html, "<h1>Markdown Markup!</h1>")
 
-    def test_markdown(self):
+    def test_textile(self):
         instance = MarkupField.objects.create(body="h1. Textile Markup!", markup="textile")
         self.assertEqual(instance.body_html, "\t<h1>Textile Markup!</h1>")
 
@@ -32,11 +32,11 @@ class ModelTest(BaseTest):
 
     #-----------------------------------------------------------------------------------
 
-    def test_textile2(self):
+    def test_markdown2(self):
         instance = MarkupField.objects.create(body="Markdown: äöüß", markup="markdown")
         self.assertEqual(instance.body_html, "<p>Markdown: äöüß</p>")
 
-    def test_markdown2(self):
+    def test_textile2(self):
         instance = MarkupField.objects.create(body="Textile äöüß", markup="textile")
         self.assertEqual(instance.body_html, "\t<p>Textile äöüß</p>")
 
